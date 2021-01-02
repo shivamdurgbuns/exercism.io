e8c945a13f180a22724adc918d85d291a43cc06d
Shivam Durgbuns <shivamdurgbuns@gmail.com>
2021-01-02 17:59:20 +0530

    [update] made changes in two fer exercise, and added side atcitiy: yatch

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -1,5 +1,2 @@
-def two_fer(name=None):
-    if name:
-        return "One for {}, one for me.".format(name)
-    else:
-        return "One for you, one for me."
+def two_fer(name="you"):
+    return "One for {}, one for me.".format(name)
